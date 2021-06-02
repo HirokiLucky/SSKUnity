e11fe88bd29349b4c11941c6beaa09260aac618b
LAPTOP-P687UVBT\81808 <nagadon0424@icloud.com>
2021-06-02 22:20:54 +0900

    マウス移動、左クリックでジャンプ

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -19,32 +19,38 @@ public class HelloWorld : MonoBehaviour
         sv.y = 1f;
         Camera.main.transform.position = sv + cv;
 
-        var v = Vector3.zero;
-        if (Input.GetKey(KeyCode.UpArrow))
+        var mp = Input.mousePosition;
+        var x = (int)(mp.x / (Screen.width / 3));
+        var y = (int) (mp.y / (Screen.height / 3));
+
+        var vx = Vector3.zero;
+        var vy = Vector3.zero;
+        var vz = Vector3.zero;
+        switch (x)
         {
-            v = new Vector3(0f, 0f, 1f);
+            case 0:
+                vx = new Vector3(-1f, 0f, 0f);
+                break;
+            case 2:
+                vx = new Vector3(1f, 0f, 0f);
+                break;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        switch (y)
         {
-            v = new Vector3(0f, 0f, -1f);
+            case 0:
+                vy = new Vector3(0f, 0f, -1f);
+                break;
+            case 2:
+                vy = new Vector3(0f, 0f, 1f);
+                break;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetMouseButtonDown(0))
         {
-            v = new Vector3(-1f, 0f, 0f);
+            vz = new Vector3(0f, 1000f, 0f);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            v = new Vector3(1f, 0f, 0f);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            v = new Vector3(0f, 10f, 0f);
-        }
-
-        rb.AddForce(v);
+        
+            rb.AddForce(vx + vy + vz);
     }
 }
