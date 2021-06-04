c4a4c831ae9384b9ba85d6ba0df44a891ad62724
LAPTOP-P687UVBT\81808 <nagadon0424@icloud.com>
2021-06-05 07:03:40 +0900

    ハローを使使用、コンポーネントからエフェクト

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
 {
     // Start is called before the first frame update
     bool f = true;
-    Vector3 cv = new Vector3(0f, 1f, -5f);
+    Vector3 cv = new Vector3(0f, 3f, -10f);
     Rigidbody rb = null;
     void Start()
     {
@@ -32,9 +32,17 @@ public class HelloWorld : MonoBehaviour
     {
         if (collider.gameObject.tag == "Other")
         {
-            var r = collider.gameObject.GetComponent<Renderer>();
-            r.material.color = new Color(0f, 0f, 0f, 0.25f);
-            r.material.SetFloat("_Metallic", 0f);
+            var h = (Behaviour) collider.gameObject.GetComponent("Halo");
+            h.enabled = true;
+        }
+    }
+    
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Other")
+        {
+            var h = (Behaviour) collider.gameObject.GetComponent("Halo");
+            h.enabled = false;
         }
     }
 }
